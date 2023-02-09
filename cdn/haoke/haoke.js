8cfbd4114eb0cc2df03742de4cdfa40ee1005140
wxydejoy <1798923421@qq.com>
2023-02-09 07:03:39 +0000

    Site updated: 2023-02-09 07:03:38

--- cdn/haoke/haoke.js
+++ cdn/haoke/haoke.js
@@ -1,12 +1,18 @@
-function _howxm(){_howxmQueue.push(arguments)}
-window._howxmQueue=window._howxmQueue||[];
-_howxm('setAppID','7dd6ab07-bc2e-4eb4-a75f-1f9f82711363');
-(function(){var scriptId='howxm_script';
-if(!document.getElementById(scriptId)){
-var e=document.createElement('script'),
-t=document.getElementsByTagName('script')[0];
-e.setAttribute('id',scriptId);
-e.type='text/javascript';e.async=!0;
-e.src='https://static.howxm.com/sdk.js';
-t.parentNode.insertBefore(e,t)}})();
+function _howxm() {
+  _howxmQueue.push(arguments);
+}
+window._howxmQueue = window._howxmQueue || [];
+_howxm("setAppID", "7dd6ab07-bc2e-4eb4-a75f-1f9f82711363");
+(function () {
+  var scriptId = "howxm_script";
+  if (!document.getElementById(scriptId)) {
+    var e = document.createElement("script"),
+      t = document.getElementsByTagName("script")[0];
+    e.setAttribute("id", scriptId);
+    e.type = "text/javascript";
+    e.async = !0;
+    e.src = "https://static.howxm.com/sdk.js";
+    t.parentNode.insertBefore(e, t);
+  }
+})();
 <link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
